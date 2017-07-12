55f992766e08b9384f152e9ecd6a48921e93b427
Jakub Liput <jakub.liput@gmail.com>
2017-07-12 11:07:04 +0200

    Config update

--- config/data_explorer/config/puma.rb
+++ config/data_explorer/config/puma.rb
@@ -1,6 +1,6 @@
 environment 'development'
 daemonize
-bind 'unix:/tmp/scalarm_experiment_supervisor.sock'
+bind 'unix:/tmp/scalarm_data_explorer.sock'
 stdout_redirect 'log/puma.log', 'log/puma.log.err', true
 pidfile 'puma.pid'
 threads 1,5
