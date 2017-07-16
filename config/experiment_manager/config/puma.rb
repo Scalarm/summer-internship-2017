e210c9bd374576ace4a4ceff47a73dedc2d7997b
Ubuntu <ubuntu@praktyki-2017-1.cloud.plgrid.pl>
2017-07-16 22:58:58 +0200

    Using Puma with SSL

--- config/experiment_manager/config/puma.rb
+++ config/experiment_manager/config/puma.rb
@@ -1,8 +1,6 @@
 environment ENV['RAILS_ENV'] or 'development'
 daemonize
-bind 'tcp://0.0.0.0:' + ENV['EXPERIMENT_MANAGER_PORT']
-## for SSH use:
-# bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'
+bind 'ssl://0.0.0.0:' + ENV['EXPERIMENT_MANAGER_PORT'] + '?key=./config/server.key&cert=./config/server.crt'
 stdout_redirect 'log/puma.log', 'log/puma.log.err', true
 pidfile 'puma.pid'
 threads 0,16
