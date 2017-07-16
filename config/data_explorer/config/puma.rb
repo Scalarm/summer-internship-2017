4afcf5f82feb6516e91a34d2541d3cb0ddab7472
Ubuntu <ubuntu@praktyki-2017-1.cloud.plgrid.pl>
2017-07-16 23:00:42 +0200

    Updating Puma configuration in services

--- config/data_explorer/config/puma.rb
+++ config/data_explorer/config/puma.rb
@@ -1,6 +1,6 @@
 environment ENV['RAILS_ENV'] or 'development'
 daemonize
-bind 'tcp://0.0.0.0:' + ENV['DATA_EXPLORER_PORT']
+bind 'ssl://0.0.0.0:' + ENV['DATA_EXPLORER_PORT'] + '?key=./config/server.key&cert=./config/server.crt'
 stdout_redirect 'log/puma.log', 'log/puma.log.err', true
 pidfile 'puma.pid'
 threads 1,5
--- config/experiment_supervisor/config/puma.rb
+++ config/experiment_supervisor/config/puma.rb
@@ -1,6 +1,6 @@
 environment ENV['RAILS_ENV'] or 'development'
 daemonize
-bind 'tcp://0.0.0.0:' + ENV['EXPERIMENT_SUPERVISOR_PORT']
+bind 'ssl://0.0.0.0:' + ENV['EXPERIMENT_SUPERVISOR_PORT'] + '?key=./config/server.key&cert=./config/server.crt'
 stdout_redirect 'log/puma.log', 'log/puma.log.err', true
 pidfile 'puma.pid'
 threads 0,16
