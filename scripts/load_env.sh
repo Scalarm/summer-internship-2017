c56d6ce17284eb479a3a9a0e1d5e9c680e625aa7
Ubuntu <ubuntu@marczak-dev.cloud.plgrid.pl>
2017-07-27 12:08:32 +0200

    Change load_env script

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -2,8 +2,8 @@
 
 # NOTE: use this script only with "source" command
 
-THIS_DIR=`dirname "$0"`
-ENV_FILE=${THIS_DIR}/../environment.env
+THIS_DIR=`dirname -- $0`
+ENV_FILE=${THIS_DIR}/environment.env
 echo "Will load env from file: ${THIS_DIR}/../environment.env ..."
 export $(cat ${ENV_FILE} | xargs)
 echo "Now check environment with \"env\" command"
